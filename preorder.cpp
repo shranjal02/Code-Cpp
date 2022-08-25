877c4a3ccc3b8cb01a057422a48a260287e504c5
Shranjal Agrawal <101035605+shranjalagrawal2@users.noreply.github.com>
2022-08-25 11:14:35 -0700

    Add files via upload

---
+++ preorder.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+Node *traverseLink(Node *root)
+{
+
+    cout << "Enter the data: " << endl;
+    int data;
+    cin >> data;
+    root = new Node(data);
+
+    if (data == -1)
+    {
+        return NULL;
+    }
+    // enter left
+    cout << " Enter for left " << data << endl;
+    root->left = traverseLink(root->left);
+    // enter right
+    cout << "Enter for right " << data << endl;
+    root->right = traverseLink(root->right);
+    return root;
+}
+void levelOrderTraversal(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+
+        if (temp == NULL)
+        {
+            cout << endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data << " ";
+
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+int main()
+{
+    Node *root = NULL;
+    root = traverseLink(root);
+    levelOrderTraversal(root);
+
+    return 0;
+}
